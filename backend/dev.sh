9a5ebfa7cfeeed5b87099c0c6efe6074a682a864
louiscklaw <louiscklaw@users.noreply.github.com>
2023-09-18 13:19:26 +0800

    update running on windows,

--- backend/dev.sh
+++ backend/dev.sh
@@ -4,4 +4,9 @@ set -ex
 
 chown 1000:1000 -R tmp
 
-./node_modules/nodemon/bin/nodemon.js --ext "*" --ignore "./tmp" --exec "npm run start"
+./node_modules/nodemon/bin/nodemon.js \
+  -L \
+  --delay 1 \
+  --ext "*" \
+  --ignore "./tmp" \
+  --exec "npm run start"
